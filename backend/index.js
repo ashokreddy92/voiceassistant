9c05b5792ff2b2866770c373be35dfbe8b6b216e
VENNAPUSA ASHOK <vennapusaashok8@gmail.com>
2025-12-30 21:58:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5174",
+    origin:"https://voiceassistant-ydzw.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
